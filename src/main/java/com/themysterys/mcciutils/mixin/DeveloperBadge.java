dd2185fed88177a05d8294c8df1c50f04554a151
TheMystery <the-mystery@outlook.com>
2022-08-28 14:12:24 +1200

    Added gem icon as dev badge
    (For people who help with mod development and will be given at my discretion)

---
+++ src/main/java/com/themysterys/mcciutils/mixin/DeveloperBadge.java
@@ -0,0 +1,35 @@
+package com.themysterys.mcciutils.mixin;
+
+import com.themysterys.mcciutils.McciUtils;
+import net.minecraft.client.gui.hud.PlayerListHud;
+import net.minecraft.client.network.PlayerListEntry;
+import net.minecraft.text.MutableText;
+import net.minecraft.text.Style;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import net.minecraft.util.Identifier;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(PlayerListHud.class)
+public class DeveloperBadge {
+
+
+    @Inject(at = @At("RETURN"), method = "getPlayerName", cancellable = true)
+    public void getDisplayName(CallbackInfoReturnable<Text> cir) {
+        if (!McciUtils.isOnMCCI()) {
+            return;
+        }
+        Text name = cir.getReturnValue();
+        if (name.getString().contains("TheMysterys")) {
+            MutableText mutableText = name.copy();
+            mutableText.append(Text.literal("\uE07D").setStyle(Style.EMPTY.withFont(new Identifier("mcc:icon"))).formatted(Formatting.WHITE));
+            cir.setReturnValue(mutableText);
+        }
+    }
+}
