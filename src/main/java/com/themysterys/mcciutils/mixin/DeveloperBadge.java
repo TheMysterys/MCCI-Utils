2ae5ed10ada04fc60ec88facb8cdb44719d3dc01
TheMysterys <the-mystery@outlook.com>
2022-08-29 18:36:45 +1200

    Puts dev badge before any extenders in users name in tab

--- src/main/java/com/themysterys/mcciutils/mixin/DeveloperBadge.java
+++ src/main/java/com/themysterys/mcciutils/mixin/DeveloperBadge.java
@@ -2,18 +2,15 @@ package com.themysterys.mcciutils.mixin;
 
 import com.themysterys.mcciutils.McciUtils;
 import net.minecraft.client.gui.hud.PlayerListHud;
-import net.minecraft.client.network.PlayerListEntry;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Style;
 import net.minecraft.text.Text;
+import net.minecraft.text.TextContent;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.Identifier;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PlayerListHud.class)
@@ -29,9 +26,15 @@ public class DeveloperBadge {
             }
         } else {
             if (name.getString().contains("TheMysterys")) {
-                MutableText mutableText = name.copy();
-                mutableText.append(Text.literal("\uE07D").setStyle(Style.EMPTY.withFont(new Identifier("mcc:icon"))).formatted(Formatting.WHITE));
-                cir.setReturnValue(mutableText);
+                MutableText newName = MutableText.of(TextContent.EMPTY);
+                for (Text value : name.getSiblings()) {
+                    if (value.getString().contains("TheMysterys")) {
+                        newName.append(value).append(Text.literal("\uE07D").setStyle(Style.EMPTY.withFont(new Identifier("mcc:icon"))).formatted(Formatting.WHITE));
+                    } else {
+                        newName.append(value);
+                    }
+                }
+                cir.setReturnValue(newName);
             }
         }
     }
