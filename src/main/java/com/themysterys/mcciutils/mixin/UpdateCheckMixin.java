fcb0cd86dc3b78090df191f307aa2dc00861160f
TheMysterys <the-mystery@outlook.com>
2022-09-02 17:59:51 +1200

    Use custom toast

--- src/main/java/com/themysterys/mcciutils/mixin/UpdateCheckMixin.java
+++ src/main/java/com/themysterys/mcciutils/mixin/UpdateCheckMixin.java
@@ -1,9 +1,9 @@
 package com.themysterys.mcciutils.mixin;
 
+import com.themysterys.mcciutils.util.McciToast;
 import com.themysterys.mcciutils.util.updates.UpdateChecker;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.multiplayer.MultiplayerScreen;
-import net.minecraft.client.toast.SystemToast;
 import net.minecraft.client.toast.ToastManager;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
@@ -18,7 +18,7 @@ public class UpdateCheckMixin {
     public void checkUpdate(CallbackInfo ci) {
         if (UpdateChecker.isUpdateAvailable()) {
             ToastManager toastManager = MinecraftClient.getInstance().getToastManager();
-            SystemToast.add(toastManager, SystemToast.Type.PERIODIC_NOTIFICATION, Text.of("New Update!"), Text.of("MCCI Utils"));
+            McciToast.add(toastManager, McciToast.Type.UPDATE, Text.of("MCCI Utils"));
         }
     }
 }
