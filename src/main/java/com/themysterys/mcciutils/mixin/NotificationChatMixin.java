942f8d909ee5379d14d236ff15f376500d27901d
TheMystery <the-mystery@outlook.com>
2022-09-07 15:24:53 +1200

    Renamed to be more informative on what this class does

--- src/main/java/com/themysterys/mcciutils/mixin/NotificationChatMixin.java
+++ src/main/java/com/themysterys/mcciutils/mixin/NotificationChatMixin.java
@@ -23,7 +23,7 @@ import java.util.regex.Pattern;
 import static com.themysterys.mcciutils.util.sounds.Sounds.*;
 
 @Mixin(ChatHud.class)
-public class ChatMixin {
+public class NotificationChatMixin {
 
     @Inject(at = @At("HEAD"), method = "addMessage(Lnet/minecraft/text/Text;)V")
     private void addMessage(Text message, CallbackInfo ci) {
