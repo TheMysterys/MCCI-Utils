dd28010b5b338ccdad8f653add298565e2a066ab
TheMystery <the-mystery@outlook.com>
2022-09-12 21:14:15 +1200

    Better location tracking

--- src/main/java/com/themysterys/mcciutils/mixin/LocationMixin.java
+++ src/main/java/com/themysterys/mcciutils/mixin/LocationMixin.java
@@ -15,25 +15,22 @@ import java.util.regex.Pattern;
 @Mixin(ScoreboardObjective.class)
 public class LocationMixin {
 
-    private static String Lastlocation;
+    private static String LastLocation;
 
     @Inject(at= @At("HEAD"), method = "setDisplayName")
     private void onScoreboardObjectiveUpdate(Text name, CallbackInfo ci) {
         if(McciUtils.isOnMCCI()) {
-            Pattern pattern = Pattern.compile("literal\\{([A-Z ]+)}", Pattern.DOTALL);
-            Matcher matcher = pattern.matcher(name.toString());
+            Pattern pattern = Pattern.compile("MCCI: (.+)", Pattern.DOTALL);
+            Matcher matcher = pattern.matcher(name.getString());
             while (matcher.find()) {
                 String location = matcher.group(1).strip();
-                if (location.length() > 0 && !location.equals(Lastlocation)) {
+                if (location.length() > 0 && !location.equals(LastLocation)) {
                     LocationID.updateLocationID(location);
-                    Lastlocation = location;
+                    LastLocation = location;
                     LocationID.sendPresence();
                     return;
                 }
             }
-            Lastlocation = null;
-            LocationID.updateLocationID(null);
-            LocationID.sendPresence();
         }
     }
 
