1895da2ddf5886c2d02fda7dfbd034c7f56237b4
TheMysterys <the-mystery@outlook.com>
2022-09-21 01:02:44 +1200

    Added sound files to mod (allows for them to be the same in all gamemodes)

--- src/main/java/com/themysterys/mcciutils/util/sounds/Sounds.java
+++ src/main/java/com/themysterys/mcciutils/util/sounds/Sounds.java
@@ -6,9 +6,9 @@ import net.minecraft.util.Identifier;
 public class Sounds {
 
     // Sound IDs
-    private static final Identifier FRIEND_SOUND_ID = new Identifier("mcc:19");
-    private static final Identifier MENTION_SOUND_ID = new Identifier("mcc:1q");
-    private static final Identifier UNLOCK_SOUND_ID = new Identifier("mcc:v");
+    private static final Identifier FRIEND_SOUND_ID = new Identifier("mcciutils:friend");
+    private static final Identifier MENTION_SOUND_ID = new Identifier("mcciutils:mention");
+    private static final Identifier UNLOCK_SOUND_ID = new Identifier("mcciutils:achievement");
 
     // Sound Events
     public static SoundEvent FRIEND_SOUND = new SoundEvent(FRIEND_SOUND_ID);
