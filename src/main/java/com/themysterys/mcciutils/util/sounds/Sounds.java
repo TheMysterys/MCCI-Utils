0abbe9ed882b85932ec0bcc98f68ee33c73ad7b3
TheMysterys <the-mystery@outlook.com>
2022-09-23 00:54:40 +1200

    Removed Sound files and changed to vanilla sounds

--- src/main/java/com/themysterys/mcciutils/util/sounds/Sounds.java
+++ src/main/java/com/themysterys/mcciutils/util/sounds/Sounds.java
@@ -1,18 +1,14 @@
 package com.themysterys.mcciutils.util.sounds;
 
 import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Identifier;
 
 public class Sounds {
 
-    // Sound IDs
-    private static final Identifier FRIEND_SOUND_ID = new Identifier("mcciutils:friend");
-    private static final Identifier MENTION_SOUND_ID = new Identifier("mcciutils:mention");
-    private static final Identifier UNLOCK_SOUND_ID = new Identifier("mcciutils:achievement");
-
     // Sound Events
-    public static SoundEvent FRIEND_SOUND = new SoundEvent(FRIEND_SOUND_ID);
-    public static SoundEvent MENTION_SOUND = new SoundEvent(MENTION_SOUND_ID);
-    public static SoundEvent UNLOCK_SOUND = new SoundEvent(UNLOCK_SOUND_ID);
+    public static SoundEvent FRIEND_SOUND = SoundEvents.BLOCK_NOTE_BLOCK_PLING;
+    public static SoundEvent MENTION_SOUND = SoundEvents.ENTITY_EXPERIENCE_ORB_PICKUP;
+    public static SoundEvent UNLOCK_SOUND = SoundEvents.ENTITY_PLAYER_LEVELUP;
 
 }
