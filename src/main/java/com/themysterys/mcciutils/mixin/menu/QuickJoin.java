d4803d9724869d20f0fe250c1d0e045f53b6bd5b
TheMysterys <the-mystery@outlook.com>
2022-09-23 08:35:31 +1200

    Adding QuickJoin join button (Placeholder file)

---
+++ src/main/java/com/themysterys/mcciutils/mixin/menu/QuickJoin.java
@@ -0,0 +1,10 @@
+package com.themysterys.mcciutils.mixin.menu;
+
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(TitleScreen.class)
+public class QuickJoin {
+
+
+}
