493a5ca456eeedd6c82d1c712b723a41cdd2e606
TheMysterys <the-mystery@outlook.com>
2022-09-26 19:30:43 +1300

    Added random chance for each background with weight

--- src/main/java/com/themysterys/mcciutils/util/panoramas/Panoramas.java
+++ src/main/java/com/themysterys/mcciutils/util/panoramas/Panoramas.java
@@ -3,7 +3,7 @@ package com.themysterys.mcciutils.util.panoramas;
 import net.minecraft.client.gui.CubeMapRenderer;
 import net.minecraft.util.Identifier;
 
-import java.util.Random;
+import java.util.*;
 
 public class Panoramas {
 
@@ -11,10 +11,7 @@ public class Panoramas {
 
 
     public Panoramas() {
-        int SIZE = PanoramaLocation.values().length;
-        Random random = new Random();
-        int index = random.nextInt(SIZE);
-        panorama = PanoramaLocation.values()[index].panorama;
+        panorama = PanoramaLocation.randomLocation();
     }
 
     public CubeMapRenderer getPanorama() {
@@ -22,12 +19,32 @@ public class Panoramas {
     }
 
     private enum PanoramaLocation {
-        MAIN_ISLAND(new CubeMapRenderer(new Identifier("mcciutils", "textures/gui/panoramas/main-island/panorama"))),
-        TGTTOS(new CubeMapRenderer(new Identifier("mcciutils", "textures/gui/panoramas/tgttos/panorama")));
+        MAIN_ISLAND(new CubeMapRenderer(new Identifier("mcciutils", "textures/gui/panoramas/main-island/panorama")), 20),
+        TGTTOS(new CubeMapRenderer(new Identifier("mcciutils", "textures/gui/panoramas/tgttos/panorama")), 20),
+        HITW(new CubeMapRenderer(new Identifier("mcciutils", "textures/gui/panoramas/hitw/panorama")), 20),
+        BB(new CubeMapRenderer(new Identifier("mcciutils", "textures/gui/panoramas/bb/panorama")), 20),
+        SB(new CubeMapRenderer(new Identifier("mcciutils", "textures/gui/panoramas/sb/panorama")), 20),
+        YELLOW(new CubeMapRenderer(new Identifier("mcciutils", "textures/gui/panoramas/yellow/panorama")), 1);
 
         final CubeMapRenderer panorama;
-        PanoramaLocation(CubeMapRenderer panorama) {
+        final int weight;
+
+        PanoramaLocation(CubeMapRenderer panorama, int weight) {
             this.panorama = panorama;
+            this.weight = weight;
+        }
+
+        private static final Random RANDOM = new Random();
+
+        public static CubeMapRenderer randomLocation()  {
+            List<PanoramaLocation> VALUES = new ArrayList<>();
+            for (PanoramaLocation value : values()) {
+                for (int i = 0; i < value.weight; i++) {
+                    VALUES.add(value);
+                }
+            }
+            int randomNum = RANDOM.nextInt(VALUES.size());
+            return VALUES.get(randomNum).panorama;
         }
     }
 }
