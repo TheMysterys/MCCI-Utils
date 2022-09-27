a8efa70c57dafa2a10a9f230c3a6674acdc0eafe
TheMysterys <the-mystery@outlook.com>
2022-09-27 19:56:00 +1300

    Renamed and added MCC Island's birthday as option

--- src/main/java/com/themysterys/mcciutils/mixin/menu/SplashSelector.java
+++ src/main/java/com/themysterys/mcciutils/mixin/menu/SplashSelector.java
@@ -1,6 +1,6 @@
 package com.themysterys.mcciutils.mixin.menu;
 
-import com.themysterys.mcciutils.McciUtils;
+import com.themysterys.mcciutils.util.api.DateAPI;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
@@ -10,13 +10,16 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
-public class MCCDay {
+public class SplashSelector {
 
     @Shadow @Nullable private String splashText;
 
     @Inject(at = @At("HEAD"), method = "init()V")
     public void isMCCDay(CallbackInfo ci) {
-        if (McciUtils.mccAPI.isMCCDay()) {
+        if (DateAPI.isMCCIBirthday()) {
+            String age = DateAPI.getMCCIAge();
+            this.splashText = "Happy %s Birthday, MCC Island!".formatted(age);
+        } else if (DateAPI.isMCCDay()) {
             splashText = "MCC Day!";
         }
     }
