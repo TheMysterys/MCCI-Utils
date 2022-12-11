3aa30e448aae3d9b03f5dcbf4e555988185c3b94
TheMysterys <the-mystery@outlook.com>
2022-12-12 10:12:01 +1300

    Possible fix for reload resource pack looping when used with Fabric Capes mod

--- src/main/java/com/themysterys/mcciutils/mixin/ingame/ElytraMixin.java
+++ src/main/java/com/themysterys/mcciutils/mixin/ingame/ElytraMixin.java
@@ -1,16 +1,21 @@
 package com.themysterys.mcciutils.mixin.ingame;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.themysterys.mcciutils.util.capes.PlayerCapes;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.client.render.entity.feature.ElytraFeatureRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(ElytraFeatureRenderer.class)
 public class ElytraMixin {
-    @Redirect(method = "render(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;ILnet/minecraft/entity/LivingEntity;FFFFFF)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/AbstractClientPlayerEntity;canRenderCapeTexture()Z"))
-    private boolean toggleCustomElytra(AbstractClientPlayerEntity abstractClientPlayerEntity) {
-        return PlayerCapes.getHasElytraTexture(abstractClientPlayerEntity.getGameProfile());
+
+    @WrapOperation(
+            method = "render",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/AbstractClientPlayerEntity;canRenderCapeTexture()Z")
+    )
+    public boolean toggleCustomElytra(AbstractClientPlayerEntity player, Operation<Boolean> ignored) {
+        return PlayerCapes.getHasElytraTexture(player.getGameProfile());
     }
 }
